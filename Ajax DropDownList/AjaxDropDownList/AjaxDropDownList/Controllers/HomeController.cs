cf095da3fbd18c2fa05e2dd3363b1e6b9e73b8cc
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2018-06-25 23:31:45 -0300

    Começando com DDL

---
+++ Ajax DropDownList/AjaxDropDownList/AjaxDropDownList/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace AjaxDropDownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<string> listaString = new List<string>();
+
+            listaString.Add("Primeiro Item");
+            listaString.Add("Segundo Item");
+            listaString.Add("Terceiro Item");
+            listaString.Add("Quarto Item");
+            listaString.Add("Quinto Item");
+
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            foreach (var itemDdl in listaString)
+            {               
+               items.Add(new SelectListItem { Text = itemDdl, Value = itemDdl });
+            }
+                                 
+            return View();
+        }
+
+        public ActionResult Cadastra(string ddl2)
+        {
+
+           
+
+            return View();
+        }
+    }
+}
---
+++ Ajax DropDownList/AjaxDropDownList/AjaxDropDownList/Models/EntidadeFormulario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxDropDownList.Models
+{
+    public class EntidadeFormulario
+    {
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string DropDown1 { get; set; }
+
+        public string DropDown2 { get; set; }
+
+        public string DropDown3 { get; set; }
+    }
+}
